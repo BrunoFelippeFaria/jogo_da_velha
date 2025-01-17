46c78ab35b9a8b78259866a3a407851e16065073
bruno <brunofelippefaria@gmail.com>
2025-01-17 15:55:24 -0300

    opção de fechar jogo após empate

--- src/Gui.java
+++ src/Gui.java
@@ -112,6 +112,10 @@ public class Gui extends JFrame implements ActionListener {
             if (escolha == 0){
                 resetarJogo();
             }
+            
+            else{
+                dispose();
+            }
         } 
     }
 
